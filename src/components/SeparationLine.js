a65fae8d2e8802e95965c29a4258582df9143e89
tobiaserich <tobiaserich@hotmail.de>
2022-05-14 15:33:12 +0200

    Add separation line as new component

---
+++ src/components/SeparationLine.js
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+const SeparationLine = styled("div")`
+  /* dividing line */
+  margin: auto;
+  width: 200px;
+  height: 0px;
+
+  border: 1px solid #bde0fe;
+`;
+
+export default SeparationLine;
