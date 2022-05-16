d6e5172cd832f1ade28013f98b5b573d375ba67d
tobiaserich <tobiaserich@hotmail.de>
2022-05-16 18:17:16 +0200

    Add burgermenu button + story

---
+++ src/stories/BurgerMenu.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Burgermenu from "../components/BurgerMenu";
+
+export default {
+  title: "components",
+  component: Burgermenu,
+};
+
+export const BurgerMenu = () => {
+  return <Burgermenu></Burgermenu>;
+};
