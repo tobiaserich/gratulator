0e385052c3a41b93c5c6db2590d3fb8f58e85ca6
tobiaserich <56834185+tobiaserich@users.noreply.github.com>
2022-05-18 19:26:11 +0200

    Add burgermenu button + story (#4)

---
+++ src/components/BurgerMenu.js
@@ -0,0 +1,110 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const SingleBurgerLine = styled("div")`
+  width: 25px;
+  height: 3px;
+  position: relative;
+  border-radius: 10px;
+  background-color: #a2d2ff;
+  animation: ${({ pos, animation }) =>
+    animation === "initial" ? "" : `${animation}${pos} 0.5s ease-in both;`};
+
+  @keyframes transformToX1 {
+    0% {
+    }
+    100% {
+      transform: rotate(45deg) translateY(10px);
+    }
+  }
+  @keyframes transformToX2 {
+    0% {
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+  @keyframes transformToX3 {
+    0% {
+    }
+    100% {
+      transform: rotate(-45deg) translateY(-10px);
+    }
+  }
+
+  @keyframes transformFromX1 {
+    0% {
+      transform: rotate(45deg) translateY(10px);
+    }
+    100% {
+      transform: rotate(0deg) translateY(0px);
+    }
+  }
+  @keyframes transformFromX2 {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+  @keyframes transformFromX3 {
+    0% {
+      transform: rotate(-45deg) translateY(-10px);
+    }
+    100% {
+      transform: rotate(0deg) translateY(0px);
+    }
+  }
+`;
+
+const BurgerContainer = styled("div")`
+  width: 25px;
+  height: 17px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  animation: ${({ animation }) =>
+    animation === "initial" ? "" : `${animation}4 0.5s ease-out both`};
+  @keyframes transformToX4 {
+    0% {
+    }
+    100% {
+      transform-origin: left;
+      transform: rotate(360deg) translateX(5px);
+    }
+  }
+  @keyframes transformFromX4 {
+    0% {
+      transform-origin: left;
+      transform: rotate(360deg) translateX(5px);
+    }
+    100% {
+      transform-origin: left;
+      transform: rotate(0deg) translateX(0px);
+    }
+  }
+`;
+const BurgerMenu = () => {
+  const [animationName, setAnimationName] = React.useState("initial");
+
+  const handleClick = () => {
+    const currentStatus = animationName;
+    const newStatus =
+      currentStatus === "initial" || currentStatus === "transformFromX"
+        ? "transformToX"
+        : "transformFromX";
+    setAnimationName(newStatus);
+    console.log(newStatus);
+  };
+
+  return (
+    <BurgerContainer animation={animationName} onClick={handleClick}>
+      <SingleBurgerLine animation={animationName} pos={1} />
+      <SingleBurgerLine animation={animationName} pos={2} />
+      <SingleBurgerLine animation={animationName} pos={3} />
+    </BurgerContainer>
+  );
+};
+
+export default BurgerMenu;
