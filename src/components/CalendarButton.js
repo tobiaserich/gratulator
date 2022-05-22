54ab3d9e6a4408cf0034e0a317c6f48e507a2d5a
tobiaserich <tobiaserich@hotmail.de>
2022-05-22 16:44:12 +0200

    Add CalendarButton component

---
+++ src/components/CalendarButton.js
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+import calendarSymbol from "../assets/calendarSymbol.svg";
+
+const CalendarButton = styled("button")`
+  width: 25px;
+  height: 25px;
+  background: #a2d2ff;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.6);
+  border-radius: 10px;
+  border: none;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-image: url("${calendarSymbol}");
+  :hover {
+    box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.6);
+  }
+  :active {
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.6);
+  }
+`;
+
+export default CalendarButton;
---
+++ src/stories/CalendarButton.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import CalendarButtonComponent from "../components/CalendarButton";
+
+export default {
+  title: "components",
+  component: CalendarButtonComponent,
+};
+
+export const CalendarButton = () => <CalendarButtonComponent />;
