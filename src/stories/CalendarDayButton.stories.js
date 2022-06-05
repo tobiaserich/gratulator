7d1e83e80d139f5f0cb7b7905c7dee85c7e4eca3
tobiaserich <56834185+tobiaserich@users.noreply.github.com>
2022-06-05 14:10:50 +0200

    Add CalendarDayButton component (#12)

---
+++ src/stories/CalendarDayButton.stories.js
@@ -0,0 +1,6 @@
+import React from "react";
+import CalendarDayButtonComponent from "../components/CalendarDayButton";
+
+export default { title: "components", component: CalendarDayButtonComponent };
+
+export const CalendarDayButton = () => <CalendarDayButtonComponent />;
