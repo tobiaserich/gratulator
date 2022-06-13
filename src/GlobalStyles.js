d830d1f75119c58e9ab0803922057459151b4d77
tobiaserich <tobiaserich@hotmail.de>
2022-06-13 15:58:53 +0200

    Change some globalstyle properties

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -13,12 +13,15 @@ function GlobalStyles({ theme }) {
         body {
           margin: 0;
           font-family: "Arima Madurai", "sans-serif";
-          background-color:${theme.colors.background}};
-          overflow:hidden;
-          height:100vh;
+          background-color: ${theme.colors.background};
+          overflow: hidden;
+          height: 100vh;
         }
-
-      }
+        #root {
+          width: 100%;
+          max-width: 500px;
+          margin: auto;
+        }
       `}
     />
   );
