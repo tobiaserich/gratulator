e3253112e3eba968dc7195991c6c09234b029867
tobiaserich <tobiaserich@hotmail.de>
2022-06-15 17:50:49 +0200

    Add AddPersonForm component also change header behaviour at scrolling

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,20 +5,46 @@ import NextBirthday from "../components/NextBirthday";
 import getNextBirthday from "../helper/getNextBirthday";
 import sortBirthdays from "../helper/sortBirthdays";
 import MainMenu from "../components/MainMenu";
+import AddPersonForm from "../components/AddPersonForm";
 
 const Header = styled("div")`
   display: flex;
   width: 100%;
+  background-color: #413a50;
   flex-direction: row;
   justify-content: center;
   align-items: center;
   text-align: center;
-  position: relative;
+  position: ${({ isSticky }) => (isSticky === true ? "sticky" : "relative")};
+  top: 0;
+  z-index: 60;
   color: #e6e6e6;
   margin-top: 0;
   margin-bottom: 0;
   font-size: 38px;
   text-shadow: 2px 2px 3px rgba(0, 0, 0, 70%);
+  animation: ${({ animate }) => animate} 0.2s ease-out;
+  transform-origin: top;
+  @keyframes moveIn {
+    0% {
+      transform: scaleY(0);
+    }
+    100% {
+      transform: scaleY(1);
+    }
+  }
+  @keyframes moveOut {
+    0% {
+      transform: scaleY(1);
+    }
+    100% {
+      transform: scaleY(0);
+    }
+  }
+`;
+
+const Container = styled("div")`
+  ${({ isFormActive }) => (isFormActive ? "height:100vh;overflow:hidden;" : "")}
 `;
 
 const birthdayList = [
@@ -74,6 +100,12 @@ const birthdayList = [
 ];
 
 const Main = () => {
+  const [addPersonFormVisible, setAddPersonFormVisible] = React.useState(true);
+  const [headerAnimation, setHeaderAnimation] = React.useState(null);
+  const [touchPosition, setTouchPosition] = React.useState(null);
+  const [isSticky, setIsSticky] = React.useState(false);
+  const [timeoutId, setTimeoutId] = React.useState();
+
   const sortedBirthdayList = sortBirthdays(birthdayList);
   const nextBirthday = getNextBirthday(sortedBirthdayList);
   const months = [
@@ -114,14 +146,63 @@ const Main = () => {
     }
     return collectMonths;
   };
+
+  const handleTouchStart = (e) => {
+    const touchDown = e.touches[0].clientY;
+    setTouchPosition(touchDown);
+  };
+
+  const handleTouchMove = (e) => {
+    const touchDown = touchPosition;
+
+    if (touchDown === null) {
+      return;
+    }
+
+    const currentTouch = e.touches[0].clientY;
+    const diff = touchDown - currentTouch;
+
+    if (diff > 15) {
+      setTimeout(() => setIsSticky(false), 200);
+
+      setHeaderAnimation("moveOut");
+    }
+    if (diff < -25) {
+      setIsSticky(true);
+      setHeaderAnimation("moveIn");
+    }
+    setTouchPosition(currentTouch);
+  };
+
+  const handleWheel = (e) => {
+    if (e.deltaY < 0) {
+      setIsSticky(true);
+      setHeaderAnimation("moveIn");
+    }
+    if (e.deltaY > 0) {
+      setTimeout(() => setIsSticky(false), 200);
+      setHeaderAnimation("moveOut");
+    }
+  };
+
   return (
-    <>
-      <Header>
-        Gratulator <MainMenu />
+    <Container
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onWheel={handleWheel}
+      isFormActive={addPersonFormVisible}
+    >
+      {addPersonFormVisible ? (
+        <AddPersonForm closeForm={() => setAddPersonFormVisible(false)} />
+      ) : (
+        ""
+      )}
+      <Header isSticky={isSticky} animate={headerAnimation}>
+        Gratulator <MainMenu setFormVisible={setAddPersonFormVisible} />
       </Header>
       <NextBirthday birthdayList={nextBirthday} />
       {IterateTroughMonths()}
-    </>
+    </Container>
   );
 };
 
