85c84548b19af1a6fa6864c41150a9d65c1ae853
tobiaserich <tobiaserich@hotmail.de>
2022-06-15 17:49:24 +0200

    Update Globalstyles

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -14,13 +14,13 @@ function GlobalStyles({ theme }) {
           margin: 0;
           font-family: "Arima Madurai", "sans-serif";
           background-color: ${theme.colors.background};
-          overflow: hidden;
           height: 100vh;
         }
         #root {
           width: 100%;
           max-width: 500px;
           margin: auto;
+          position: relative;
         }
       `}
     />
