1517423af81d8e31a8a2b1cee770101bb8ce8d16
tobiaserich <56834185+tobiaserich@users.noreply.github.com>
2022-06-30 14:38:32 +0200

    Validation hook (#18)

--- src/components/ButtonBar.js
+++ src/components/ButtonBar.js
@@ -4,10 +4,10 @@ const ButtonBar = styled("div")`
   display: flex;
   margin-top: ${({ errorMessageActive }) => {
     let sum = 40;
-    if (!errorMessageActive.name) {
+    if (!errorMessageActive.name && errorMessageActive.name !== null) {
       sum = sum - 8;
     }
-    if (!errorMessageActive.date) {
+    if (!errorMessageActive.date && errorMessageActive.date !== null) {
       sum = sum - 32;
     }
     return `${sum}px;`;
